f53056ddbcb1877ea1ff24e4da0ccf436fad1bc0
radpi42 <67181776+radpi42@users.noreply.github.com>
2020-06-23 14:00:02 -0700

    Add files via upload

---
+++ connect-check.py
@@ -0,0 +1,28 @@
+import connection
+import time
+
+
+counter_mes = '''times run: %d'''
+count = 0
+
+def connect():
+    try:
+        connection.check()
+        return True
+    except:
+        return False
+
+
+while True:
+        
+    while connection.check() == True:   
+        print(connection.check())
+        count +=1
+
+        print(counter_mes %count)
+        time.sleep(1)
+    else:
+        print('no connection')
+        count +=1
+        print(counter_mes %count)
+        time.sleep(1)
